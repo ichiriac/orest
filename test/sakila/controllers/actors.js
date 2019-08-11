9fe8a64ecb00e14e0b282a06aadd20781f99c5ec
Ioan CHIRIAC <i.chiriac@yahoo.com>
2019-08-11 11:48:41 +0200

    start to describe the api

--- test/sakila/controllers/actors.js
+++ test/sakila/controllers/actors.js
@@ -1,3 +1,54 @@
 module.exports = function(api, db) {
-    // @todo
+    const model = db.model('Actor');
+    const films = db.model('Film');
+    const actors = api.entity(model);
+
+    // handles the listing
+    actors.list(function(req, res, filter) {
+        return model.findAll(filter);
+    });
+    // or for default : actors.list(true);
+
+    // enables the search options
+    actors.search(['first_name', 'last_name']);
+
+    /**
+     * Most films
+     */
+    actors.filter('most-films', function() {
+
+    }).describe('Authors ordered by count of films');
+
+    /**
+     * Filter by films categories
+     */
+    actors.filter('with-category-:category', function() {
+
+    }).describe('Authors that played on the specified film category');
+
+    /**
+     * List authors by sales
+     */
+    actors.filter('bankable', function() {
+
+    }).describe('Authors ordered by number of generated rentals');
+
+    // crud actions
+    actors.create(true);
+    actors.read(true);
+    actors.update(true);
+    actors.delete(true);
+    // alias : actors.crud(true);
+
+    actors.bulkUpdate(true);
+    actors.bulkDelete(function(req, res, filter) {
+        return model.deleteAll(filter);
+    });
+    // alias : actors.bulk(true);
+
+    // relations
+    actors.read('/films', function(req, res, item) {
+        return films.findAll(filter);
+    });
+
 };
