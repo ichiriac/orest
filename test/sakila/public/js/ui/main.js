564cc6c5778adb6b039dde13593ead900ed40bbe
Ioan CHIRIAC <i.chiriac@yahoo.com>
2020-02-24 22:21:39 +0100

    add a logout btn

--- test/sakila/public/js/ui/main.js
+++ test/sakila/public/js/ui/main.js
@@ -1,6 +1,19 @@
 (function($) {
     sakila.ui.main = function($app, api) {
         $app.empty();
-        // todo :
+        $app.append(
+            $('<div class="card card-container">').append([
+                $('<button class="btn btn-danger">').text('logout').on('click', function() {
+                    // request to logout
+                    api.logout().then(function() {
+                        sakila.ui.login($app, api, function() {
+                            sakila.ui.main($app, api);
+                        });
+                    }).catch(function(e) {
+                        alert(e.message);
+                    })
+                })
+            ])
+        );
     };
 })(jQuery);
